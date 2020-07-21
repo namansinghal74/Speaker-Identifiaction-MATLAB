b9b19af675339b3ee9656e531bf48465e1be858a
namansinghal74 <35928471+namansinghal74@users.noreply.github.com>
2020-07-21 15:21:13 +0530

    Add files via upload

---
+++ trainClassifier.m
@@ -0,0 +1,53 @@
+function [trainedClassifier, validationAccuracy] = trainClassifier(trainingData)
+inputTable = trainingData;
+% Split matrices in the input table into vectors
+inputTable = [inputTable(:,setdiff(inputTable.Properties.VariableNames, {'feat'})), array2table(table2array(inputTable(:,{'feat'})), 'VariableNames', {'feat_1', 'feat_2', 'feat_3', 'feat_4', 'feat_5', 'feat_6', 'feat_7', 'feat_8', 'feat_9', 'feat_10', 'feat_11', 'feat_12', 'feat_13', 'feat_14', 'feat_15', 'feat_16', 'feat_17', 'feat_18', 'feat_19', 'feat_20', 'feat_21', 'feat_22', 'feat_23', 'feat_24', 'feat_25', 'feat_26', 'feat_27', 'feat_28'})];
+
+predictorNames = {'feat_1', 'feat_2', 'feat_3', 'feat_4', 'feat_5', 'feat_6', 'feat_7', 'feat_8', 'feat_9', 'feat_10', 'feat_11', 'feat_12', 'feat_13', 'feat_14', 'feat_15', 'feat_16', 'feat_17', 'feat_18', 'feat_19', 'feat_20', 'feat_21', 'feat_22', 'feat_23', 'feat_24', 'feat_25', 'feat_26', 'feat_27', 'feat_28'};
+predictors = inputTable(:, predictorNames);
+response = inputTable.lab;
+isCategoricalPredictor = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+
+classificationKNN = fitcknn(...
+    predictors, ...
+    response, ...
+    'Distance', 'Euclidean', ...
+    'Exponent', [], ...
+    'NumNeighbors', 10, ...
+    'DistanceWeight', 'SquaredInverse', ...
+    'Standardize', true, ...
+    'ClassNames', categorical({'Naman'; 'id 1'; 'id 2'; 'id 3'; 'id 4'; 'id 5'; 'id 6'; 'id 7'; 'id 8'; 'id 9'}));
+
+% Create the result struct with predict function
+splitMatricesInTableFcn = @(t) [t(:,setdiff(t.Properties.VariableNames, {'feat'})), array2table(table2array(t(:,{'feat'})), 'VariableNames', {'feat_1', 'feat_2', 'feat_3', 'feat_4', 'feat_5', 'feat_6', 'feat_7', 'feat_8', 'feat_9', 'feat_10', 'feat_11', 'feat_12', 'feat_13', 'feat_14', 'feat_15', 'feat_16', 'feat_17', 'feat_18', 'feat_19', 'feat_20', 'feat_21', 'feat_22', 'feat_23', 'feat_24', 'feat_25', 'feat_26', 'feat_27', 'feat_28'})];
+extractPredictorsFromTableFcn = @(t) t(:, predictorNames);
+predictorExtractionFcn = @(x) extractPredictorsFromTableFcn(splitMatricesInTableFcn(x));
+knnPredictFcn = @(x) predict(classificationKNN, x);
+trainedClassifier.predictFcn = @(x) knnPredictFcn(predictorExtractionFcn(x));
+
+% Add additional fields to the result struct
+trainedClassifier.RequiredVariables = {'feat'};
+trainedClassifier.ClassificationKNN = classificationKNN;
+trainedClassifier.About = 'This struct is a trained model exported from Classification Learner R2019b.';
+trainedClassifier.HowToPredict = sprintf('To make predictions on a new table, T, use: \n  yfit = c.predictFcn(T) \nreplacing ''c'' with the name of the variable that is this struct, e.g. ''trainedModel''. \n \nThe table, T, must contain the variables returned by: \n  c.RequiredVariables \nVariable formats (e.g. matrix/vector, datatype) must match the original training data. \nAdditional variables are ignored. \n \nFor more information, see <a href="matlab:helpview(fullfile(docroot, ''stats'', ''stats.map''), ''appclassification_exportmodeltoworkspace'')">How to predict using an exported model</a>.');
+
+% Extract predictors and response
+% This code processes the data into the right shape for training the
+% model.
+inputTable = trainingData;
+% Split matrices in the input table into vectors
+inputTable = [inputTable(:,setdiff(inputTable.Properties.VariableNames, {'feat'})), array2table(table2array(inputTable(:,{'feat'})), 'VariableNames', {'feat_1', 'feat_2', 'feat_3', 'feat_4', 'feat_5', 'feat_6', 'feat_7', 'feat_8', 'feat_9', 'feat_10', 'feat_11', 'feat_12', 'feat_13', 'feat_14', 'feat_15', 'feat_16', 'feat_17', 'feat_18', 'feat_19', 'feat_20', 'feat_21', 'feat_22', 'feat_23', 'feat_24', 'feat_25', 'feat_26', 'feat_27', 'feat_28'})];
+
+predictorNames = {'feat_1', 'feat_2', 'feat_3', 'feat_4', 'feat_5', 'feat_6', 'feat_7', 'feat_8', 'feat_9', 'feat_10', 'feat_11', 'feat_12', 'feat_13', 'feat_14', 'feat_15', 'feat_16', 'feat_17', 'feat_18', 'feat_19', 'feat_20', 'feat_21', 'feat_22', 'feat_23', 'feat_24', 'feat_25', 'feat_26', 'feat_27', 'feat_28'};
+predictors = inputTable(:, predictorNames);
+response = inputTable.lab;
+isCategoricalPredictor = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+
+% Perform cross-validation
+partitionedModel = crossval(trainedClassifier.ClassificationKNN, 'KFold', 5);
+
+% Compute validation predictions
+[validationPredictions, validationScores] = kfoldPredict(partitionedModel);
+
+% Compute validation accuracy
+validationAccuracy = 1 - kfoldLoss(partitionedModel, 'LossFun', 'ClassifError');
